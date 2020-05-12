ff24bc8ccca0d39a97d721a4d5646bce9e80fecd
ehsyb <ehsyb@naver.com>
2020-05-12 09:55:41 +0900

    초기 커밋.

---
+++ startree/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace startree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 8; i++)
+            {;
+                for(int j = 1;j<= 15; j++)
+                {
+                    if (j <= 8 - i)
+                    {
+                        Console.Write(" ");
+
+                    }
+                    if (j >= 8 + i)
+                    {
+                        Console.Write(" ");
+                    }
+                    if (j > 8 - i && j < 8 + i)
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
